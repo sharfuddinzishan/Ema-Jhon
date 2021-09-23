e5d4b5863e47c880fa61571afbba5899f4e8859b
sharfuddinzishan <sharfuddin.ahamed91@gmail.com>
2021-09-23 18:12:24 +0600

    [Added] Cart Stored in LocalStorage

--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css'
 import Product from './../Product/Product';
 import fakeData from '../../fakeData/product-data';
 import Cart from '../Cart/Cart';
 
 const Shop = () => {
-    let sliceProducts = fakeData.slice(0, 10);
     // const [items, setItems] = useState(sliceProducts);
+    // let [carts, setCarts] = useState([]);
+    // let addToCart = product => {
+    //     let newCart = [...carts, product];
+    //     setCarts(newCart);
+    // };
+    let sliceProducts = fakeData.slice(0, 10);
     let [carts, setCarts] = useState([]);
+    useEffect(() => {
+        if (!localStorage.getItem('cart')) {
+            localStorage.setItem('cart', JSON.stringify(carts))
+        }
+        else {
+            setCarts(JSON.parse(localStorage.getItem('cart')))
+        }
+    }, [])
+
     let addToCart = product => {
         let newCart = [...carts, product];
+        localStorage.setItem('cart', JSON.stringify(newCart))
         setCarts(newCart);
-    };
+    }
     return (
         <div id="shop-container">
             <div className="products-container">
-
                 {
                     sliceProducts.map((pd) => <Product product={pd} key={pd.key} addToCart={addToCart}></Product>)
                 }
